e3a335ed07521ac7fd91df2dcea6c117b7be0f01
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-01-21 21:06:36 -0400

    Ajuste da responsividade da página de edição de notícias

--- view/assets/js/main.js
+++ view/assets/js/main.js
@@ -14,12 +14,19 @@ const box = document.querySelector(".fotos-slide");
 // const imagens = document.querySelectorAll(".fotos-slide img");
 
 let contador = 0;
+let tela = window.innerWidth;
+let slideWidth = 0;
+if(tela <= 481){
+    slideWidth = 200;
+}else{
+    slideWidth = 1016;
+}
 function slider(){
     contador++;
     if (contador > 4) {
         contador=0;
     }
-    box.style.transform = `translateX(${-contador * 1016}px)`;
+    box.style.transform = `translateX(${-contador * slideWidth}px)`;
 }
 
 
