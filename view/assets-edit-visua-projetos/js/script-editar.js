8083d9462e6c225562730d92a9e484cef01b4ddb
FilipeMatins <filipemarcorreia@gmail.com>
2025-02-12 21:07:16 -0400

    telas projetos ong finalizadas

--- view/assets-edit-visua-projetos/js/script-editar.js
+++ view/assets-edit-visua-projetos/js/script-editar.js
@@ -4,6 +4,8 @@ const salvarAlteracaoProjeto = document.getElementById('salvar-alteracao-projeto
 const telaDeEdicao = document.getElementById('tela-confirma-edicao');
 const inativarProjeto = document.getElementById('inativar-projeto');
 const TelaInativaProjeto = document.getElementById('tela-inativa-projeto');
+const btnInativa = document.getElementById('btn-inativa');
+const confirmacaoInativa = document.getElementById('confirmacao-inativa');
 
 
 
@@ -19,11 +21,22 @@ inativarProjeto.onclick = function(){
     TelaInativaProjeto.style.display = "block";
 }
 
+btnInativa.onclick = function() {
+    confirmacaoInativa.style.display = "block";
+}
+
 window.onclick = function(event) {
     if (event.target == telaDeEdicaoProjeto) {
         telaDeEdicaoProjeto.style.display = "none";
     }
     if (event.target == telaDeEdicao) {
         telaDeEdicao.style.display = "none";
+    }
+    if (event.target == TelaInativaProjeto) {
+        TelaInativaProjeto.style.display = "none";
+    }
+    if (event.target == confirmacaoInativa) {
+        confirmacaoInativa.style.display = "none";
+        TelaInativaProjeto.style.display = "none";
     }
 }
