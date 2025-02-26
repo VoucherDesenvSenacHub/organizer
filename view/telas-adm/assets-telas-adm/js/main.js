e55005edfc4cf6c1e9df46f52655c19b694d6624
BrunaBorgs <brunaaacb@gmail.com>
2025-02-25 21:10:33 -0400

    alterando popups

--- view/telas-adm/assets-telas-adm/js/main.js
+++ view/telas-adm/assets-telas-adm/js/main.js
@@ -14,8 +14,8 @@ window.addEventListener('resize', () => {
     }
 });
 
-function loginPopup() {
-    const fundoPopup = document.getElementById('login-popup');
+function blockpopup() {
+    const fundoPopup = document.getElementById('block-popup');
     const btnNao = document.getElementById('btn-nao')
     fundoPopup.classList.add('ativo');
 
@@ -32,9 +32,9 @@ function loginPopup() {
     });
 }
 
-function blockpopup() {
-    const fundoPopupBlock = document.getElementById('login-popup');
-    const fundoPopup = document.getElementById('block-popup')
+function blockpopup2() {
+    const fundoPopupBlock = document.getElementById('block-popup');
+    const fundoPopup = document.getElementById('block-popup2')
     fundoPopup.classList.add('ativo');
 
     fundoPopup.addEventListener('click', (event) => {
