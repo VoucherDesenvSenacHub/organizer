a919e91e293658fcd632846ddf56584f9130541b
TawanyDuda <eduarda50438216@aluno.ms.senac.br>
2025-03-07 21:13:14 -0400

    atualizando

--- view/pages/ONG/relatorio.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $tituloPagina = ''; // Definir o título da página
-    $cssPagina = []; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
-    require_once '../../components/header.php';
-?>
-<!-- COMEÇO DO CÓDIGO AQUI -->
