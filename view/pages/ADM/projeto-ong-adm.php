ed1a134b8834b6862cba3beb143d8f45c136af49
GiovanaGomessz <giovanavgf2008@gmail.com>
2025-03-11 19:39:29 -0400

    Atualizando código para php

---
+++ view/pages/ADM/projeto-ong-adm.php
@@ -0,0 +1,311 @@
+<?php 
+    $tituloPagina = 'Projetos da ONG'; // Definir o título da página
+    $cssPagina = ['ADM/projeto-ong-adm.css']; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
+    require_once '../../components/header.php';
+?>
+
+    <script src="main.js">
+    </script>
+
+    <div id="principal">
+
+        <div class="card">
+
+            <div class="cima">
+                <div class="titulo">
+                    <h1>Campanha Solidária</h1>
+                    <h2 style="margin-top: 5px;">Arrecadado: R$20.000</h2>
+                    <div class="barra">
+                        <div class="verde"></div>
+                    </div>
+                    <h3 >Meta: 
+                        <span class="detalhe">R$100.000</span>
+                    </h3>
+                    <h3>Status: Em progresso 
+                        <span class="detalhe">(20% alcançado)</span>
+                    </h3>
+                    <h3>
+                        <span class="detalhe">24</span>
+                        Doações Recebidas
+                    </h3>
+
+                    <button class="botao">Inativar
+                        <img src="../../assets/images/lixeira.png">
+                    </button>
+                </div>
+
+                <div class="img">
+                    <img class="imagemONG" src="../../assets/images/c.-solidario.png">
+                </div>
+            </div>
+
+            <div class="meio">
+                <div class="sobre">
+                    <h1>Sobre</h1>
+                    <img src="../../assets/images/interrogação.png" >
+                </div>
+
+                <p class="criacao">Ong criada em: 25/12/2023</p>
+
+                <div class="letra">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur qui quasi accusamus ipsa inventore deserunt
+                    perspiciatis unde dolorum reiciendis officia?Lorem ipsum 
+                    dolor sit amet consectetur adipisicing elit. Consequuntur
+                    qui quasi accusamus ipsa inventore deserunt perspiciatis 
+                    unde dolorum reiciendis officia?
+                </div>
+            </div>
+
+            <div class="baixo">
+                <div class="doadores">
+                    <h1>Doadores</h1>
+                    <img src="../../assets/images/doadores.png" >
+                </div>
+
+                <div class="lista_doadores">
+                    <div class="usuario">
+                        <img src="../../assets/images/joao.png">
+                        <p class="margin_usuario">João</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/maria.png">
+                        <p class="margin_usuario">Maria</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/pedro.png">
+                        <p class="margin_usuario">Pedro</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/joao.png">
+                        <p class="margin_usuario">João</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/maria.png">
+                        <p class="margin_usuario">Maria</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/pedro.png">
+                        <p class="margin_usuario">Pedro</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/joao.png">
+                        <p class="margin_usuario">João</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/maria.png">
+                        <p class="margin_usuario">Maria</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/pedro.png">
+                        <p class="margin_usuario">Pedro</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/joao.png">
+                        <p class="margin_usuario">João</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/maria.png">
+                        <p class="margin_usuario">Maria</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/pedro.png">
+                        <p class="margin_usuario">Pedro</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/joao.png">
+                        <p class="margin_usuario">João</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/maria.png">
+                        <p class="margin_usuario">Maria</p>
+                        <span>R$102</span>
+                    </div>
+
+                    <div class="usuario">
+                        <img src="../../assets/images/pedro.png">
+                        <p class="margin_usuario">Pedro</p>
+                        <span>R$102</span>
+                    </div>
+                    
+                </div>
+
+                <div class="menu">
+                    <button class="um">1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>></button>
+                </div>
+            </div>
+
+            <div class="baixo">
+                <div class="voluntarios">
+                    <h1>Voluntários</h1>
+                    <img src="../../assets/images/voluntario.png" >
+                </div>
+
+                <div class="lista_voluntarios">
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/pedro.png">
+                        <div class="volun">
+                            <p>Pedro</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/maria.png">
+                        <div class="volun">
+                            <p>Maria</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/joao.png">
+                        <div class="volun">
+                            <p>João</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/pedro.png">
+                        <div class="volun">
+                            <p>Pedro</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/maria.png">
+                        <div class="volun">
+                            <p>Maria</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/joao.png">
+                        <div class="volun">
+                            <p>João</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/pedro.png">
+                        <div class="volun">
+                            <p>Pedro</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/maria.png">
+                        <div class="volun">
+                            <p>Maria</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/joao.png">
+                        <div class="volun">
+                            <p>João</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/pedro.png">
+                        <div class="volun">
+                            <p>Pedro</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/maria.png">
+                        <div class="volun">
+                            <p>Maria</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/joao.png">
+                        <div class="volun">
+                            <p>João</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/pedro.png">
+                        <div class="volun">
+                            <p>Pedro</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/maria.png">
+                        <div class="volun">
+                            <p>Maria</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+
+                    <div class="quadradinhos">
+                        <img src="../../assets/images/joao.png">
+                        <div class="volun">
+                            <p>João</p>
+                            <span>Apoia o projeto tal</span>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="menu">
+                    <button class="um">1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>></button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php
+    $jsPagina = ['main.js']; //Colocar o arquivo .js (exemplo: 'cadastro.js')
+    require_once '../../components/footer.php';
+?>
