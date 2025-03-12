a931f87ec7129a67c44b8fb5536f42cd94cb8a04
TawanyDuda <eduarda50438216@aluno.ms.senac.br>
2025-03-11 21:41:23 -0400

    iniciando 11.03.25

--- view/pages/ONG/relatorios.php
+++ view/pages/ONG/relatorios.php
@@ -3,6 +3,75 @@
     $cssPagina = ["ONG/relatorios.css"]; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
     require_once '../../components/header.php';
 ?>
+ <!-- <main>
+        <section>
+            <div id="principal">
+                <div class="topo-relatorios">
+                    <h1>RELATÓRIOS</h1>
+                </div>
+                <div class="conteudo-relatorios">
+                    <div class="relatorio-voluntarios">
+                        <div class="topo-grafico">
+                            <h2>Voluntários por Projeto</h2>
+                            <button>
+                                <img src="../../assets/images/icon-download-report.png" alt="">
+                            </button>
+                        </div>
+                            <img src="../../assets/images/graph-vol-proj.png" alt="">
+                        <span class="rodape-grafico">
+                            <img src="../assets/images/SquareFill.png" alt="">
+                            <p>Voluntários</p>
+                        </span>
+                    </div>
+                    <div class="relatorio-doacoes-mensais">
+                        <div class="topo-grafico">
+                            <h2>Doações Mensais</h2>
+                            <button>
+                                <img src="../assets/images/icon-download-report.png" alt="">
+                            </button>
+                        </div>
+                        <img src="../../assets/images/montly-donation.png" alt="">
+                        <span class="rodape-grafico">
+                            <img src="../assets/images/donation.png" alt="">
+                            <p>Doações</p>
+                        </span>
+                    </div>
+                    <div class="relatorio-doacoes-projeto">
+                        <div class="topo-grafico">
+                            <h2>Doações por Projeto</h2>
+                            <button>
+                                <img src="../../assets/images/icon-download-report.png" alt="">
+                            </button>
+                        </div>
+                        <img src="../../assets//images/pie-graph.png" alt="" id="pie-graph">
+                        <img src="../assets//images/legend-pie-graph.png" alt="" id="legend-pie-graph">
+                    </div>
+                    <div class="relatorio-doacoes-voluntario">
+                        <div class="topo-grafico">
+                            <h2>Doações/Voluntários por Projeto</h2>
+                            <button>
+                                <img src="../../assets/images/icon-download-report.png" alt="">
+                            </button>
+                        </div>
+                        <img src="../assets/images/yAxisTop.png" alt="" id="axis">
+                        <img src="../assets/images/volunt-doac-relat.png" alt="">
+                        <span class="rodape-grafico">
+                            <img src="..//assets/images/greenSquare.png" alt="">
+                            <p>Doações</p>
+                            <img src="../../assets/images/blueSquare.png" alt="">
+                            <p>Voluntários</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+     -->
+
+
+
+
     <main>
         <div id="principal">
             <div class="titulo">
@@ -12,18 +81,20 @@
             <div class="cards">
                 <div class="card1">
                     <div class="icon">
-                        Projetos ativos/inativos
+                    Voluntários por Projeto
                         <button onclick="clicar()"><img src="../../assets/images/icon.png" alt=""></button>
+                        
                     </div>
-                    <img src="../../assets/images/grafico1.png" alt="grafico">
+                    <img src="../../assets/images/icon-download-report.png" alt="">
                     <div class="sub">
-                        <img src="../../assets/images/Legends1.png" alt="">
+                        <img src="../../assets/images/graph-vol-proj.png" alt="">
+                        <img src="../assets/images/SquareFill.png" alt="">
                     </div>
                 </div>
 
                 <div class="card1">
                     <div class="icon">
-                        Doações
+                        Doações Mensais
                         <button onclick="clicar()"><img src="../../assets/images/icon.png" alt=""></button>
                     </div>
                     <img src="../../assets/images/grafico2.png" alt="">
@@ -34,18 +105,15 @@
 
                 <div class="card1">
                     <div class="icon">
-                        Acessos no sistema
+                        Doações por projeto
                         <button onclick="clicar()"><img src="../../assets/images/icon.png" alt=""></button>
                     </div>
-                    <img src="../../assets/images/grafico3.png" alt="">
-                    <div class="sub">
-                        <img src="../../assets/images/Legends3.png" alt="">
-                    </div>
+                    <img src="../../assets//images/pie-graph.png" alt="" id="pie-graph">
                 </div>
 
                 <div class="card1">
                     <div class="icon">
-                        Categorias das ONGS
+                        Doações/Voluntários por Projeto
                         <button onclick="clicar()"><img src="../../assets/images/icon.png" alt=""></button>
                     </div>
                     <img src="../../assets/images/grafico4.png" alt="">
@@ -53,7 +121,7 @@
             </div>
 
             <div id="download">
-                <img src="a../../assets/images/icon.png" alt="">
+                <img src="../../assets/images/icon.png" alt="">
                 <p>Download Iniciado</p>
             </div>
 
