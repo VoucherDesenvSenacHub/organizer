28195b47e84d356e865490df6789e5ef252ffc9d
Geanoff <geancorrea.ga@gmail.com>
2025-03-17 21:22:18 -0400

    Adicionando pagina não encontrada

---
+++ view/pages/not-found.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página em Andamento</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: aliceblue;
+        }
+
+        main .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        main h1 {
+            color: darkslategray;
+        }
+
+        main img {
+            width: 400px;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>Em desenvolvimento...</h1>
+            <img src="../assets/images/global/tela-em-andamento.png" alt="">
+        </div>
+    </main>
+</body>
+</html>
