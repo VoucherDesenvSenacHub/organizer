43e968f748b65d4900224eb71f2f1080f96e7f5e
TawanyDuda <eduarda50438216@aluno.ms.senac.br>
2025-03-18 21:46:08 -0400

    Iniciando tela para validação de ongs 18.03.25

---
+++ view/pages/adm/ongs-a-serem-validadas.php
@@ -0,0 +1,22 @@
+<?php 
+    $tituloPagina = 'Validação de Ongs'; // Definir o título da página
+    $cssPagina = ["adm/ongs-a-serem-validadas.css"]; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
+    require_once '../../components/header.php';
+?>
+
+<!-- INICIO CÓDIGO -->
+
+<main>
+    <section>
+        <div class="principal">
+            <h1>oi</h1>
+        </div>
+    </section>
+</main>
+
+
+
+<?php
+    $jsPagina = []; //Colocar o arquivo .js (exemplo: 'cadastro.js')
+    require_once '../../components/footer.php';
+?>
