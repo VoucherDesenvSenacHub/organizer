c39a9760cd90a205d918f96c2472e39df233b5b8
danieldmota <danieldourado2102@gmail.com>
2025-03-18 21:46:44 -0400

    começando tela participacoes

---
+++ view/pages/doador/participacoes.php
@@ -0,0 +1,23 @@
+<?php 
+    $tituloPagina = 'Participações'; // Definir o título da página
+    $cssPagina = ['doador/participacoes.css']; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
+    require_once '../../components/header.php';
+?>
+
+<main>
+    <section>
+        <div class="container">
+            <div id="secao-1">
+                <h1>SUAS PARTICIPAÇÕES</h1>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+    $jsPagina = []; //Colocar o arquivo .js (exemplo: 'cadastro.js')
+    require_once '../../components/footer.php';
+?>
