2506385d63c6ce9c167bf54f4e5048d08b9ac711
Geanoff <geancorrea.ga@gmail.com>
2025-03-20 13:50:23 -0400

    Correção responsividade ong e projeto

--- view/pages/visitante/projetos.php
+++ view/pages/visitante/projetos.php
@@ -13,84 +13,86 @@
                     <p>Explore projetos inspiradores e apoie causas e faça a diferença hoje mesmo.</p>
                     <form id="form-filtro" action="projetos.php" method="GET">
                         <!-- ### -->
-                        <ul class="drop" id="esc-status">
-                            <li>
-                                <p>Status</p>
-                                <i class="fa-solid fa-angle-down"></i>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="em-andamento" id="em-andamento">
-                                <label for="em-andamento">Em andamento</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="concluido" id="concluido">
-                                <label for="concluido">Concluído</label>
-                            </li>
-                        </ul>
-                        <ul class="drop" id="esc-categoria">
-                            <li>
-                                <p>Categoria</p>
-                                <i class="fa-solid fa-angle-down"></i>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="educacao" id="educacao">
-                                <label for="educacao">Educação</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="saude" id="saude">
-                                <label for="saude">Saúde</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="esporte" id="esporte">
-                                <label for="esporte">Esporte</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="cultura" id="cultura">
-                                <label for="cultura">Cultura</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="tecnologia" id="tecnologia">
-                                <label for="tecnologia">Tecnologia</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="ambiente" id="ambiente">
-                                <label for="ambiente">Meio Ambiente</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="animal" id="animal">
-                                <label for="animal">Proteção Animal</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="direitos" id="direitos">
-                                <label for="direitos">Direitos Humanos</label>
-                            </li>
-                        </ul>
-                        <ul class="drop" id="esc-regiao">
-                            <li>
-                                <p>Região</p>
-                                <i class="fa-solid fa-angle-down"></i>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="centro-oeste" id="centro-oeste">
-                                <label for="centro-oeste">Centro-Oeste</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="norte" id="norte">
-                                <label for="norte">Norte</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="nordeste" id="nordeste">
-                                <label for="nordeste">Nordeste</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="sudeste" id="sudeste">
-                                <label for="sudeste">Sudeste</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" name="sul" id="sul">
-                                <label for="sul">Sul</label>
-                            </li>
-                        </ul>
+                        <div class="ul-group">
+                            <ul class="drop" id="esc-status">
+                                <li>
+                                    <p>Status</p>
+                                    <i class="fa-solid fa-angle-down"></i>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="em-andamento" id="em-andamento">
+                                    <label for="em-andamento">Em andamento</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="concluido" id="concluido">
+                                    <label for="concluido">Concluído</label>
+                                </li>
+                            </ul>
+                            <ul class="drop" id="esc-categoria">
+                                <li>
+                                    <p>Categoria</p>
+                                    <i class="fa-solid fa-angle-down"></i>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="educacao" id="educacao">
+                                    <label for="educacao">Educação</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="saude" id="saude">
+                                    <label for="saude">Saúde</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="esporte" id="esporte">
+                                    <label for="esporte">Esporte</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="cultura" id="cultura">
+                                    <label for="cultura">Cultura</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="tecnologia" id="tecnologia">
+                                    <label for="tecnologia">Tecnologia</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="ambiente" id="ambiente">
+                                    <label for="ambiente">Meio Ambiente</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="animal" id="animal">
+                                    <label for="animal">Proteção Animal</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="direitos" id="direitos">
+                                    <label for="direitos">Direitos Humanos</label>
+                                </li>
+                            </ul>
+                            <ul class="drop" id="esc-regiao">
+                                <li>
+                                    <p>Região</p>
+                                    <i class="fa-solid fa-angle-down"></i>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="centro-oeste" id="centro-oeste">
+                                    <label for="centro-oeste">Centro-Oeste</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="norte" id="norte">
+                                    <label for="norte">Norte</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="nordeste" id="nordeste">
+                                    <label for="nordeste">Nordeste</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="sudeste" id="sudeste">
+                                    <label for="sudeste">Sudeste</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="sul" id="sul">
+                                    <label for="sul">Sul</label>
+                                </li>
+                            </ul>
+                        </div>
                         <button class="btn">Filtrar</button>
                     </form>
                 </div>
