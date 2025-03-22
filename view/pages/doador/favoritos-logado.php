80659a12dcf80260f5956711b12f2466d611c4c0
Vitor Ribeiro <vitor54099266@aluno.ms.senac.br>
2025-03-21 21:40:30 -0400

    alteração sextinhaaa

--- view/pages/doador/favoritos-logado.php
+++ view/pages/doador/favoritos-logado.php
@@ -12,8 +12,8 @@
                 <h1>MEUS FAVORITOS</h1>
                 
                     <div id="buttons">
-                        <button class="rs">ONGS <img src= "../../assets/images/medalha-icon.png" alt=""> </button>
-                        <button class="rs">PROJETO <img src="../../assets/images/medalha-icon.png" alt=""></button>
+                        <button class="rs" onclick="ongs()">ONGS<img src= "../../assets/images/medalha-icon.png" alt=""></button>
+                        <button class="rs" onclick="projetos()">PROJETO<img src="../../assets/images/medalha-icon.png" alt=""></button>
                     </div>
 
                 <div id="principal">
@@ -246,11 +246,134 @@
                             </div>
                         </div>
                     <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+
+                
                 </div>
                 
+            
+        
+                <div class="card-projeto">
+                    <div class="acoes-projeto">
+                        <button id="share" class="fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+                        <button id="like" class="fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
+                    </div>
+                    <div class="img-projeto">250x130</div>
+                        <div class="info-projeto">
+                            <h5>Nome Projeto</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum et? Repellendus perferendis provident
+                            ratione deserunt alias cumque et quasi odio amet temporibus, quam obcaecati dolores. Enim quibusdam a atque.
+                                </p>
+                                <div class="barra-doacao">
+                                    <span>30%</span>
+                                    <div class="barra">
+                                        <div class="barra-verde"></div>
+                                    </div>
+                                </div>
+                        </div>
+                    <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+                </div>
+
+                <div class="card-projeto">
+                    <div class="acoes-projeto">
+                        <button id="share" class="fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+                        <button id="like" class="fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
+                    </div>
+                    <div class="img-projeto">250x130</div>
+                        <div class="info-projeto">
+                            <h5>Nome Projeto</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum et? Repellendus perferendis provident
+                            ratione deserunt alias cumque et quasi odio amet temporibus, quam obcaecati dolores. Enim quibusdam a atque.
+                            </p>
+                             <div class="barra-doacao">
+                                    <span>30%</span>
+                                <div class="barra">
+                                    <div class="barra-verde"></div>
+                                </div>
+                            </div>
+                         </div>
+                    <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+                </div>
+
+                <div class="card-projeto">
+                    <div class="acoes-projeto">
+                        <button id="share" class="fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+                        <button id="like" class="fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
+                    </div>
+                        <div class="img-projeto">250x130</div>
+                        <div class="info-projeto">
+                            <h5>Nome Projeto</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum et? Repellendus perferendis provident
+                            ratione deserunt alias cumque et quasi odio amet temporibus, quam obcaecati dolores. Enim quibusdam a atque.
+                            </p>
+                             <div class="barra-doacao">
+                                <span>30%</span>
+                                    <div class="barra">
+                                        <div class="barra-verde"></div>
+                                    </div>
+                            </div>
+                        </div>
+                    <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+                </div>
+
+                <div class="card-projeto">
+                    <div class="acoes-projeto">
+                        <button id="share" class="fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+                        <button id="like" class="fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
+                    </div>
+                        <div class="img-projeto">250x130</div>
+                        <div class="info-projeto">
+                            <h5>Nome Projeto</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum et? Repellendus perferendis provident
+                            ratione deserunt alias cumque et quasi odio amet temporibus, quam obcaecati dolores. Enim quibusdam a atque.
+                            </p>
+                             <div class="barra-doacao">
+                                <span>30%</span>
+                                    <div class="barra">
+                                        <div class="barra-verde"></div>
+                                    </div>
+                            </div>
+                        </div>
+                    <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+                </div>
+
+                <div class="card-projeto">
+                    <div class="acoes-projeto">
+                        <button id="share" class="fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+                        <button id="like" class="fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
+                    </div>
+                        <div class="img-projeto">250x130</div>
+                        <div class="info-projeto">
+                            <h5>Nome Projeto</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum et? Repellendus perferendis provident
+                            ratione deserunt alias cumque et quasi odio amet temporibus, quam obcaecati dolores. Enim quibusdam a atque.
+                            </p>
+                             <div class="barra-doacao">
+                                <span>30%</span>
+                                    <div class="barra">
+                                        <div class="barra-verde"></div>
+                                    </div>
+                            </div>
+                        </div>
+                    <a class="saiba-mais-projeto" href="tela-de-projeto.php">Saiba Mais</a>
+                </div>
+                
+                <div class="btn-paginacao"> 
+                    <button class="bts">1</button>
+                    <button class="bts">2</button>
+                    <button class="bts">3</button>
+                    <button class="bts">4</button>
+                    <button class="bts">></button>
+                </div>
         
             
         </div>
+                
+        
+            
+
+            </div>
+
+        
                     </div>
                 </div>
                 
@@ -341,7 +464,7 @@
     </main> 
 
 <?php
-    $jsPagina = []; //Colocar o arquivo .js
+    $jsPagina = ['favoritos-logado.js']; //Colocar o arquivo .js
     require_once '../../components/footer.php';
 ?>
 
