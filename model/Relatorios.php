3f0b0e7f1843da5ca1506d23449b89cfce0e0eff
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-04-10 21:31:56 -0400

    Primeiro commit - funcionalidade de responsividade dos gráficos dos relatórios

---
+++ model/Relatorios.php
@@ -0,0 +1,25 @@
+<?php 
+$voluntarios = [
+    ["Projeto A", 21],
+    ["Projeto B", 29],
+    ["Projeto C", 36],
+    ["Projeto D", 40],
+    ["Projeto E", 62]
+];
+
+$doacoesMensais = [
+    ["Janeiro", 220],
+    ["Fevereiro", 240],
+    ["Março", 250],
+    ["Abril", 230],
+    ["Maio", 200],
+    ["Junho", 350],
+    ["Julho", 960],
+    ["Agosto", 470],
+    ["Setembro", 300],
+    ["Outubro", 485],
+    ["Novembro", 740],
+    ["Dezembro", 720]
+];
+
+?>
--- view/pages/adm/relatorios.php
+++ view/pages/adm/relatorios.php
@@ -2,6 +2,7 @@
     $tituloPagina = 'Relatórios'; // Definir o título da página
     $cssPagina = ["adm/relatorios.css"]; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
     require_once '../../components/header-adm.php';
+    require_once '../../../model/Relatorios.php';
 ?>
     <main>
         <div id="principal">
@@ -16,8 +17,38 @@
                         <button onclick="clicar()"><img src="../../assets/images/icon-download-report.png" alt=""></button>
                         
                     </div>
+                    <div class="grafico">
+                        <div class="eixo-y">
+                            <p>100</p>
+                            <p>75</p>
+                            <p>50</p>
+                            <p>25</p>
+                            <p>0</p>
+                        </div>
+                        <div class="grade">
+                            <div class="linha"></div>
+                            <div class="linha"></div>
+                            <div class="linha"></div>
+                            <div class="linha"></div>
+                        </div>
+                    </div>
+                    <div class="barras-verticais">
+                        <?php foreach($voluntarios as $vol){
+                        $altura = $vol[1] * 200 / 100?>
+                        <div class="barra" style="height: <?php echo ($altura)?>px;">
+                            <p><?= $vol[1]?></p>
+                        </div>
+                        <?php } ?>
+                    </div>
+                    <div class="rodape-grafico">
+                        <?php foreach($voluntarios as $vol){?>
+                        <div>
+                            <p><?php echo($vol[0]);?> </p>
+                        </div>
+                        <?php }?>
+                    </div>
 
-                    <div class="sub">
+                    <!-- <div class="sub">
                         
                         <img src="../../assets/images/graph-vol-proj.png" alt="">
 
@@ -26,7 +57,7 @@
                             <p>voluntarios</p>
                         </div>
 
-                    </div>
+                    </div> -->
                 </div>
 
                 <div class="card1">
