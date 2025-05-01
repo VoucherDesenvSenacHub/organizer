6e1accfe866f2c72aa5aa92833fb5d85057ef662
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-04-30 21:35:37 -0400

    Progresso em relatórios

--- view/pages/adm/relatorios.php
+++ view/pages/adm/relatorios.php
@@ -16,37 +16,35 @@
                     <div class="icon">
                     Voluntários por Projeto
                         <button onclick="clicar()"><img src="../../assets/images/icon-download-report.png" alt=""></button>
+                    </div>
+                    <div class="graficos">
+                        <svg style = "width: 600px; height: 320px; background: #9a9a9a; opacity: 0.3;">
+                            <?php
+                                $indices = [100, 75, 50, 25, 0];
+                                $mi = 0;
+                                for($i = 1; $i <=293; $i+=73){?>
+                                <line x1="40" y1="<?= $i ?>" x2="600" y2="<?= $i ?>" style="stroke: black; stroke-dasharray: 4 "/> <!-- Traça as linhas horizontais -->
+                                <text x="0" y="<?= $i+15 ?>"><?=$indices[$mi]?></text>
+                            <?php
+                                $mi++;
+                                };
+                                ?>
+                            <line x1="40" y1="0" x2="40" y2="290" style="stroke: black; stroke-dasharray: 4 "/>
+                            <line x1="599" y1="0" x2="599" y2="290" style="stroke: black; stroke-dasharray: 4 "/>
+                                <?php
+                                    $pontoX = 40;
+                                    $divisoes = (540/sizeof($voluntarios));
+                                    for($i = 0; $i<sizeof($voluntarios); $i++){
+                                        $pontoY = 290 - (($voluntarios[$i][1]*290)/100);?>
+                            <line x1="<?=$pontoX?>" y1="290"
+                            x2="<?=$pontoX?>"y2 ="<?=$pontoY?>"
+                            style="stroke: blue; stroke-width: 40px"/>
+                            <text x="<?=$pontoX?>" y="320"><?=$voluntarios[$i][0]?></text>
+                                <?php
+                                    $pontoX = $pontoX+($pontoX*$divisoes);
+                                }?>
                         
-                    </div>
-                    <div class="grafico">
-                        <div class="eixo-y">
-                            <p>100</p>
-                            <p>75</p>
-                            <p>50</p>
-                            <p>25</p>
-                            <p>0</p>
-                        </div>
-                        <div class="grade">
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                        </div>
-                    </div>
-                    <div class="barras-verticais">
-                        <?php foreach($voluntarios as $vol){
-                        $altura = $vol[1] * 280 / 100?>
-                        <div class="barra" style="height: <?php echo ($altura)?>px;">
-                            <p><?= $vol[1]?></p>
-                        </div>
-                        <?php } ?>
-                    </div>
-                    <div class="rodape-grafico">
-                        <?php foreach($voluntarios as $vol){?>
-                        <div>
-                            <p><?php echo($vol[0]);?> </p>
-                        </div>
-                        <?php }?>
+                        </svg>
                     </div>
                 </div>
             <!-- Fim voluntários por Projeto -->
@@ -57,52 +55,45 @@
                         Doações Mensais
                         <button onclick="clicar()"><img src="../../assets/images/icon-download-report.png" alt=""></button>
                     </div>
-                    <div class="grafico">
-                        <div class="eixo-y">
-                            <p>960</p>
-                            <p>720</p>
-                            <p>480</p>
-                            <p>240</p>
-                            <p>0</p>
-                        </div>
-                        <div class="grade">
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                            <div class="linha"></div>
-                        </div>
-                    </div>
                     <div class="grafico-linhas">
-                        <svg width="550" height="280" style="z-index: 100;">
-                        <?php
-                            $x1 = 20;
-                            $x2 = $x1+45;
+                        <svg style = "width: 600px; height: 320px">
+                            <?php
+                                $indices = [960, 720, 480, 240, 0];
+                                $mi = 0;
+                                for($i = 1; $i <=293; $i+=73){?>
+                                <line x1="40" y1="<?= $i ?>" x2="600" y2="<?= $i ?>" style="stroke: black; stroke-dasharray: 4 "/> <!-- Traça as linhas horizontais -->
+                                <text x="0" y="<?= $i+15 ?>"><?=$indices[$mi]?></text>
+                            <?php
+                                $mi++;
+                                };
                             for($i = 0; $i < sizeof($doacoesMensais); $i++){
-                            $localPonto = 280 - (280 * $doacoesMensais[$i][1]/960);
+                                $localTexto = ($i*46.66)+40;                                
+                                ?>
+                            <text x="<?= $localTexto ?>" y="315" textlenght="7"><?=$doacoesMensais[$i][0] ?></text>
+                            <line x1="<?= $localTexto ?>" y1="1" x2="<?= $localTexto ?>" y2="293" style="stroke: black; stroke-dasharray: 4 "/>
+                            <?php }?>
+                            <line x1="599" y1="1" x2="599" y2="293" style="stroke: black; stroke-dasharray: 4 "/>
+                        <?php
+                            $x1 = 60;
+                            $x2 = $x1+46.66;
+                            for($i = 0; $i < sizeof($doacoesMensais); $i++){
+                            $localPonto = 293 - (293 * $doacoesMensais[$i][1]/960);
                             if($i == sizeof($doacoesMensais)-1){
                                 $localPonto2 = $localPonto;
                                 $x2 = $x1;
                             }else{
-                                $localPonto2 = 280 - (280 * $doacoesMensais[$i+1][1]/960);
+                                $localPonto2 = 293 - (293 * $doacoesMensais[$i+1][1]/960);
                             }
                             ?>
                                 <line x1="<?=$x1?>" y1="<?=$localPonto?>" x2="<?=$x2?>" y2="<?=$localPonto2?>" style="stroke: blue; stroke-width: 2;"/>
                                 <circle r="4" cx="<?=$x1?>" cy="<?=$localPonto?>" fill="blue"/>
                                 
                                 <?php
-                                    $x1 += 45;
-                                    $x2 += 45;
+                                    $x1 += 46.66;
+                                    $x2 += 46.66;
                                 }
                                 ?>
                         </svg>
-
-                    </div>
-                    <div class="rodape-grafico">
-                        <?php foreach($doacoesMensais as $mes){?>
-                        <div>
-                            <p><?php echo($mes[0]);?> </p>
-                        </div>
-                        <?php }?>
                     </div>
                 </div>
             <!-- Fim doações Mensais -->
@@ -114,14 +105,14 @@
                         Doações por projeto
                         <button onclick="clicar()"><img src="../../assets/images/icon-download-report.png" alt=""></button>
                     </div>
-                    <div>
+                    <!-- <div>
                         <svg width="600" height="300">
                             <path style="stroke: #000; fill: none;" d="M0,0 c150,0 300,150 300,300
                             
                             "/>
                             <circle cx="300" cy="150" r="145" style="stroke: #000; fill: none;"/>
                         </svg>
-                    </div>
+                    </div> -->
                     <!-- <img src="../../assets//images/pie-graph.png" alt="" id="pie-graph"> -->
                 </div>
             <!-- Fim doações por projeto -->
