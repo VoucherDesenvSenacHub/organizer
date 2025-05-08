ad90dbd41113f61de019e13235117a0b57216d0c
GiovanaGomessz <giovanavgf2008@gmail.com>
2025-05-07 21:09:49 -0400

    revisando responsividade

--- view/pages/doador/perfil-noticia.php
+++ view/pages/doador/perfil-noticia.php
@@ -21,7 +21,7 @@
 
         <div class="texto">
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus convallis molestie commodo.
-                 Donec cursus ex vitae erat eleifend viverra. Sed luctus metus at nisl interdum suscipit. Ut venenatis aliquet lacus ut placerat.
+                Donec cursus ex vitae erat eleifend viverra. Sed luctus metus at nisl interdum suscipit. Ut venenatis aliquet lacus ut placerat.
                 Morbi bibendum,nulla feugiat laoreet cursus, purus sem vestibulum massa, laoreet rutrum justo diam non urna. Nulla facilisi. 
                  Suspendisse hendrerit nunc vehicula sem mattis tincidunt. Nulla facilisi. Mauris suscipit lorem vitae mi cursus, at laoreet urna elementum. 
                  Proin vel tellus eu massa iaculis cursus. Aliquam ultrices enim et gravida scelerisque. Sed et molestie eros. Duis sit amet velit tincidunt,
