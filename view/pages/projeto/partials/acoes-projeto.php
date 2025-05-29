e04ccb48c6a6af475a135bfda5d8f78c91782780
Geanoff <geancorrea.ga@gmail.com>
2025-05-29 17:22:30 -0400

    Ações do adm no projeto corrigido

--- view/pages/projeto/partials/acoes-projeto.php
+++ view/pages/projeto/partials/acoes-projeto.php
@@ -13,8 +13,14 @@ $perfil = $_SESSION['perfil_usuario'] ?? 'visitante';
         <button class="btn" id="btn-inativar" onclick="abrir_popup('inativar-projeto-popup')">
             <i class="fa-solid fa-trash-can"></i> Inativar
         </button>
-    <?php elseif ($perfil === 'admin'): ?>
-        <button class="btn" onclick="abrir_popup('popup-gerenciar')">Gerenciar Projeto</button>
+    <?php elseif ($perfil === 'adm'): ?>
+        <button class="btn" onclick="abrir_popup('#')">Gerenciar Projeto</button>
+        <!-- <button class="btn" id="btn-editar" onclick="abrir_popup('editar-projeto-popup')">
+            <i class="fa-solid fa-pen-to-square"></i> Editar
+        </button>
+        <button class="btn" id="btn-inativar" onclick="abrir_popup('inativar-projeto-popup')">
+            <i class="fa-solid fa-trash-can"></i> Inativar
+        </button> -->
     <?php else: ?>
         <button class="btn" id="btn-doacao" onclick="abrir_popup('login-obrigatorio-popup')">Fazer uma doação</button>
         <button class="btn" id="btn-voluntario" onclick="abrir_popup('login-obrigatorio-popup')">Ser Voluntário</button>
--- view/pages/projeto/partials/popups-projeto.php
+++ view/pages/projeto/partials/popups-projeto.php
@@ -1,9 +1,18 @@
 <?php
 $perfil = $_SESSION['perfil_usuario'] ?? 'visitante';
-if ($perfil === 'ong') {
-    require_once __DIR__ . '/../../../components/popup/formulario-projeto.php';
-    require_once __DIR__ . '/../../../components/popup/inativar-projeto.php';
-} else if ($perfil === 'doador') {
-    require_once __DIR__ . '/../../../components/popup/ser-voluntario.php';
-    require_once __DIR__ . '/../../../components/popup/doacao.php';
+switch ($perfil) {
+    case 'doador':
+        require_once __DIR__ . '/../../../components/popup/ser-voluntario.php';
+        require_once __DIR__ . '/../../../components/popup/doacao.php';
+        break;
+    case 'ong':
+        require_once __DIR__ . '/../../../components/popup/formulario-projeto.php';
+        require_once __DIR__ . '/../../../components/popup/inativar-projeto.php';
+        break;
+    case 'adm':
+        require_once __DIR__ . '/../../../components/popup/formulario-projeto.php';
+        require_once __DIR__ . '/../../../components/popup/inativar-projeto.php';
+        break;
+    default:
+        break;
 }
