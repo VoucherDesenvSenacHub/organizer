dfd25bafcbd41aeb1559830bac545e4aeb275533
Geanoff <geancorrea.ga@gmail.com>
2025-06-10 10:47:24 -0400

    Pequenas correções

--- view/pages/visitante/login.php
+++ view/pages/visitante/login.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 ob_start();
-    $tituloPagina = 'Login | Organizer';
-    $cssPagina = ['visitante/login.css'];
-    require_once '../../components/layout/base-inicio.php';
+$tituloPagina = 'Login | Organizer';
+$cssPagina = ['visitante/login.css'];
+require_once '../../components/layout/base-inicio.php';
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        require_once __DIR__ . '/../../../model/UsuarioModel.php';
-        $usuarioModel = new Usuario();
+    require_once __DIR__ . '/../../../model/UsuarioModel.php';
+    $usuarioModel = new Usuario();
 
-        $usuarioModel->login($email, $senha);
-    }
+    $usuarioModel->login($email, $senha);
+}
 ob_end_flush();
 ?>
 <main>
@@ -26,10 +26,8 @@ ob_end_flush();
                 </div>
                 <div class="input-item">
                     <label for="senha">Senha<span>*</span></label>
-                    <div class="password-container">
-                        <input id="senha" name="senha" type="password" minlength="8" maxlength="20" placeholder="********" required>
-                        <i id="togglePassword" class="fas fa-eye"></i>
-                    </div>
+                    <input id="senha" name="senha" type="password" minlength="8" maxlength="20" placeholder="********" required>
+                    <i id="togglePassword" class="fas fa-eye"></i>
                 </div>
                 <div class="remember-forgot">
                     <div class="remember">
@@ -42,7 +40,7 @@ ob_end_flush();
             <button class="btn">ENTRAR</button>
             <span>Não tem uma conta? <a href="cadastro.php">Criar Conta</a></span>
         </form>
-            <img alt="Celular-Login" src="../../assets/images/pages/login_doador.png">
+        <img alt="Celular-Login" src="../../assets/images/pages/login_doador.png">
     </div>
 </main>
 <div id="fundo-cadastro-popup" class="popup-fundo">
@@ -79,6 +77,6 @@ ob_end_flush();
     Login necessário para continuar!
 </div>
 <?php
-    $jsPagina = ['login-doador.js'];
-    require_once '../../components/footer.php';
+$jsPagina = ['login-doador.js'];
+require_once '../../components/footer.php';
 ?>
