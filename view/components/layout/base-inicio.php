2ba955d74fb054c8d4631916607cdfd38352f1db
Geanoff <geancorrea.ga@gmail.com>
2025-06-10 10:48:56 -0400

    Pequenas correções

--- view/components/layout/base-inicio.php
+++ view/components/layout/base-inicio.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Título da aba (dinâmico) -->
     <title><?= isset($tituloPagina) ? $tituloPagina : 'Sem Nome'; ?></title>
-    <!-- Icones -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="../../assets/images/global/Logo-Organizer.png">
     <!-- CSS global do sistema -->
