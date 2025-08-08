ceaad0569989544113d1e05fb634c973caf2b4e9
TawanyDuda <eduarda50438216@aluno.ms.senac.br>
2025-08-07 21:37:04 -0400

    iniciando o compartilhar do card de projetos

--- view/components/cards/card-projeto.php
+++ view/components/cards/card-projeto.php
@@ -11,7 +11,10 @@ $logo_url = $projeto->logo_url ?? '../../assets/images/global/image-placeholder.
 
 <div class="card-projeto <?= $class ?>">
     <div class="acoes-projeto">
-        <button class="btn-share fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+        <form action="">
+            <input type="text" value ="<?= $id ?>" name="projeto_id" >
+            <button class="btn-share fa-solid fa-share-nodes" onclick="abrir_popup('compartilhar-popup')"></button>
+        </form>
         <?php if (!isset($_SESSION['usuario']['id'])): ?>
             <button title="Favoritar" class="btn-like fa-solid fa-heart" onclick="abrir_popup('login-obrigatorio-popup')"></button>
         <?php else: ?>
