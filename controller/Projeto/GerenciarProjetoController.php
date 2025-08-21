cfe3dec503445bf62e34d2d0abe5ba67e460d2c9
Geanoff <geancorrea.ga@gmail.com>
2025-08-21 19:49:14 -0400

    Ajeitando o editar projeto

--- controller/Projeto/GerenciarProjetoController.php
+++ controller/Projeto/GerenciarProjetoController.php
@@ -6,16 +6,16 @@ AuthService::verificaLoginOng();
 $projetoModel = new Projeto();
 
 // Pegar os dados
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-$descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
-$meta = filter_input(INPUT_POST, 'meta', FILTER_VALIDATE_FLOAT);
+$NomeProjeto = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$DescricaoProjeto = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
+$MetaProjeto = filter_input(INPUT_POST, 'meta', FILTER_VALIDATE_FLOAT);
 
 // Criar um Projeto
 if (!$_POST['projeto-id']) {
-    $ong = $_SESSION['ong_id'];
+    $IdOng = $_SESSION['ong_id'];
 
-    if ($nome && $descricao && $meta) {
-        $sucesso = $projetoModel->criar($nome, $descricao, $meta, $ong);
+    if ($NomeProjeto && $DescricaoProjeto && $MetaProjeto) {
+        $sucesso = $projetoModel->criar($NomeProjeto, $DescricaoProjeto, $MetaProjeto, $IdOng);
         if ($sucesso) {
             $_SESSION['criar-projeto'] = true;
             header('Location: ' . $_SERVER['HTTP_REFERER']);
@@ -29,13 +29,13 @@ if (!$_POST['projeto-id']) {
 
 // Editar um Projeto
 else {
-    $projetoId = $_POST['projeto-id'];
-    $valor_arrecadado = $projetoModel->buscarValor($projetoId);
-    if ($meta < $valor_arrecadado) {
+    $IdProjeto = $_POST['projeto-id'];
+    $ValorArrecadado = $_POST['valor-arrecadado'];
+    if ($MetaProjeto < $ValorArrecadado) {
         echo "<script>alert('Meta inválida: o valor deve ser maior do que o que já foi arrecadado.');window.history.back();</script>";
         exit;
     } else {
-        $edicao = $projetoModel->editar($projetoId, $nome, $descricao, $meta);
+        $edicao = $projetoModel->editar($IdProjeto, $NomeProjeto, $DescricaoProjeto, $MetaProjeto);
         if ($edicao) {
             $_SESSION['editar-projeto'] = true;
         } else {
