1f8354d94d955520e7c6f8a6190429d69d859e84
Geanoff <geancorrea.ga@gmail.com>
2025-08-20 21:39:02 -0400

    Corrigindo o caminho do banco

--- config/database.php
+++ config/database.php
@@ -1,16 +1,16 @@
 <?php 
     // BANCO DE DADO LOCAL:
-        $host = 'localhost';
-        $port = 3306;
-        $dbname = 'organizer';
-        $user = 'root';
-        $password = '';
+        // $host = 'localhost';
+        // $port = 3306;
+        // $dbname = 'organizer';
+        // $user = 'root';
+        // $password = '';
 
-    // $host = 'centerbeam.proxy.rlwy.net';
-    // $port = 28838;
-    // $dbname = 'organizer';
-    // $user = 'root';
-    // $password = 'GxbLQsKICwpVBaOQegbVxZvLHfgSIorV'; 
+    $host = 'centerbeam.proxy.rlwy.net';
+    $port = 28838;
+    $dbname = 'organizer';
+    $user = 'root';
+    $password = 'GxbLQsKICwpVBaOQegbVxZvLHfgSIorV'; 
     
     try {
         $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
