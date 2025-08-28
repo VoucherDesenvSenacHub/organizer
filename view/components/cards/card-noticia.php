ccc35f93f57748584d48af7fd85980da770f0dd9
FilipeMatins <filipemarcorreia@gmail.com>
2025-08-27 21:38:10 -0400

    ajustando erro no card noticias

--- view/components/cards/card-noticia.php
+++ view/components/cards/card-noticia.php
@@ -1,13 +1,12 @@
 <?php
-if (!isset($noticiaModel)) {
-    require_once __DIR__ . '../../model/NoticiaModel.php';
-    $noticiaModel = new NoticiaModel();
-}
+
 // Pegar os dados da Notícia e tratar possíveis erros
 
 $IdNoticia = $noticia['noticia_id'] ?? null;
 $TituloNoticia = $noticia['titulo'] ?? 'Titulo Da Matéria';
-$TextoNoticia = mb_strimwidth($noticia['texto'], 0, 150, '...') ?? 'Lorem ipsum...';
+$TextoNoticia = (isset($noticia) && is_array($noticia) && isset($noticia['texto']))
+    ? mb_strimwidth($noticia['texto'], 0, 150, '...')
+    : 'Lorem ipsum...';
 $NomeOng = $noticia['ong_nome'] ?? 'Nome da Ong';
 $DataNoticia = $noticia['data_cadastro'] ?? '00/00/0000';
 
@@ -16,7 +15,7 @@ $imagens = $noticiaModel->buscarImagensNoticia($IdNoticia);
 $FotoNoticia = $imagens[0]['caminho'] ?? '../../assets/images/global/image-placeholder.svg';
 ?>
 
-?>
+
 <div class="card-noticia">
     <a href="../noticia/perfil.php?id=<?= $IdNoticia ?>" class="card-noticia">
         <div class="textos">
