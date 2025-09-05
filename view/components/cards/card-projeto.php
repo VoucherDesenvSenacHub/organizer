18ef1a8cbcaab5c1eefd723be38910379349afdb
Geanoff <geancorrea.ga@gmail.com>
2025-09-05 19:42:43 -0400

    Terminando o categorias no card projeto

--- view/components/cards/card-projeto.php
+++ view/components/cards/card-projeto.php
@@ -6,7 +6,7 @@ $NomeProjeto = $projeto['nome'] ?? 'Nome do Projeto';
 $DescricaoProjeto =  mb_strimwidth($projeto['descricao'], 0, 220, '...') ?? 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, explicabo magni? Laboriosam possimus voluptas recusandae blanditiis architecto dolorem tenetur odio, nisi molestiae facere quia facilis officia cumque dicta impedit minima.';
 $BarraProjeto = $projeto['barra'] ?? '30';
 $CategoriaProjeto = $projeto['categoria'] ?? 'Inválido';
-$CorCategoria = $projeto['cor'] ?? '#606060';
+$CorCategoria = $projeto['cor'] ?? '#9E9E9E';
 // Verificar se o Doador favoritou o Projeto
 $jaFavoritado = in_array($projeto['projeto_id'], $projetosFavoritos ?? []) ?? false;
 $classe = $jaFavoritado ? 'favoritado' : '';
