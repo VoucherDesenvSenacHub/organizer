027be4490e1d6f5d31d8c283d584994aa76eb09b
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-09-05 20:12:44 -0400

    Instalação do DomPDF

--- view/components/reports-pdf/voluntarios-por-projeto.php
+++ view/components/reports-pdf/voluntarios-por-projeto.php
@@ -1,12 +1,5 @@
-
-
-
 <?php
 $acesso = 'ong';
-$tituloPagina = 'Relatórios | Organizer'; // Definir o título da página
-$cssPagina = ["ong/relatorios.css"]; //Colocar o arquivo .css (exemplo: 'ONG/cadastro.css')
-require_once '../../components/layout/base-inicio.php';
-require_once '../../components/popup/download.php';
 require_once '../../components/graphics/vertical-bars.php';
 require_once '../../components/graphics/line-graphic.php';
 require_once '../../components/graphics/horizontal-double-bars.php';
@@ -26,19 +19,61 @@ $listagem_projetos = $projetos->listarProjetos(3);
 // echo "<pre>";
 // print_r($listagem_projetos);
 // echo "</pre>";
+$pagina = "
+
+";
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            width: 800px;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1em;
+        }
+        table {
+            text-align: left;
+        }
+        tr:nth-child(odd) {
+            background-color: #a8a7a5;
+        }
+        tr:nth-child(odd) {
+            background-color: #dedcd7;
+        }
+        #grafico {
+            display: grid;
+            place-items: center;
+            font-size: 0.5em;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
 <h1>Relatório de apoiadores</h1>
 <hr>
-<?php 
+<div id="grafico">
+<?php
+    if($listagem_projetos === "Não há projetos ativos cadastrados para essa ONG"){
+        echo '<h1>Não há projetos ativos cadastrados para essa ONG</h1>';
+    }else {
+
     echo graficoBarrasVerticais(600, 320, $contagem_projetos);
     echo '<hr>';
     $contador = 0;
     foreach ($contagem_projetos as $cp){ ?>
+    </div>
+    <hr>
     <h2><?php echo $cp[0] ?></h2>
+    <hr>
     <table>
         <thead>
-            <th><td>Apoiadores</td></th>
-        </thead>
+            <th>Apoiadores</th>
+        </thead>        
         <tbody>
             <?php foreach($listagem_projetos as $lp) {
                 if($cp[0] == $lp['nome_projeto']){ ?>
@@ -57,4 +92,4 @@ $listagem_projetos = $projetos->listarProjetos(3);
         </tfoot>
     </table>
     <hr>
-    <?php } ?>
+    <?php }} ?>
