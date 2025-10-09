7c362e0ddc78b1fb7cf948b959c4efbd35643833
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-10-08 21:25:29 -0400

    Alterações 08/10/2025

--- view/components/popup/doacao.php
+++ view/components/popup/doacao.php
@@ -1,4 +1,3 @@
-<!-- Adercio -->
 <div class="popup-fundo" id="doacao-popup">
     <div class="container-popup">
         <button class="btn-fechar-popup fa-solid fa-xmark" onclick="fechar_popup('doacao-popup')"></button>
@@ -29,8 +28,53 @@
                     <span>R$</span>
                     <input type="number" name="valor-personalizado" placeholder="Outro Valor">
                 </label>
+            <!-- </div> -->
+            <!-- <div class="popup-fundo" id="popup-adicionar-cartao">
+                <div class="credit-card-popup"> -->
+            <div>
+                <div>
+                    <div class="input-grupo">
+                        <label>Número do Cartão</label>
+                        <input id="number-cartao" type="text" placeholder="0000 0000 0000 0000" class="card-number">
+                    </div>
+    
+                    <div class="row">
+                        <div class="input-grupo">
+                            <label for="">Validade</label>
+                            <input id="validade-cartao" type="text" placeholder="MM/AA">
+                        </div>
+    
+                        <div class="input-grupo">
+                            <label>CVV</label>
+                            <input id="CVV" type="text" placeholder="CVV" class="cvv">
+                        </div>
+                    </div>
+    
+                    <div class="input-grupo">
+                        <label for="name">Titular do cartão</label>
+                        <input type="text" placeholder="Nome impresso no cartão" maxlength="200" onkeypress="return /[a-z\s]/i.test(event.key)">
+                    </div>
+                    <div>
+                        <button class="add-button" id="addButton">
+                            <span class="button-text">CONFIRMAR</span>
+                            <div class="loader"></div>
+                            <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                                <circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none" />
+                                <path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <button type="submit" class="btn">FAZER DOAÇÃO</button>
+        </div>
+            <button type="submit" class="btn" onclick="abrir_popup('efetuar-pagamento')">PAGAR COM CARTÃO</button>
         </form>
     </div>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+<script type="text/javascript">
+    $("#number-cartao").mask("0000 0000 0000 0000");
+    $("#CVV").mask("000");
+    $("#validade-cartao").mask("00/00");
+</script>
