8894976344705f394e31fae3babda8aba0fc4b37
Luizfelipereal007 <lmarianobonfim@gmail.com>
2025-10-16 21:17:39 -0400

    feat(ong): :hammer: add condition to prevent inactivating inactive ONG

--- view/pages/ong/partials/acoes-ong.php
+++ view/pages/ong/partials/acoes-ong.php
@@ -7,7 +7,7 @@ $perfil = $_SESSION['perfil_usuario'] ?? 'visitante';
         <a href="conta.php"><button class="btn" id="btn-editar"><i class="fa-solid fa-pen-to-square"></i> Editar</button></a>
         <button class="btn" id="btn-inativar"><i class="fa-solid fa-ban"></i> Inativar</button>
     </div>
-<?php elseif ($perfil === 'adm'): ?>
+<?php elseif ($perfil === 'adm' && $PerfilOng['status'] !== 'INATIVO'): ?>
     <div id="acoes">
         <button class="btn adm-inativar" id="btn-inativar" onclick="abrir_popup('inativar-ong-popup')"><i class="fa-solid fa-ban"></i> Inativar</button>
     </div>
