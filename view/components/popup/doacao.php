db09b2860097cc7f02ebc501d91226d70d5b7172
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-10-20 14:47:28 -0400

    Alterações 20/10/2025

--- view/components/popup/doacao.php
+++ view/components/popup/doacao.php
@@ -31,38 +31,45 @@
             <!-- </div> -->
             <!-- <div class="popup-fundo" id="popup-adicionar-cartao">
                 <div class="credit-card-popup"> -->
-            <div>
-                <div>
+            <div id="popup-adicionar-cartao">
+                <div class="credit-card-popup">
                     <div class="input-grupo">
                         <label>Número do Cartão</label>
-                        <input id="number-cartao" type="text" placeholder="0000 0000 0000 0000" class="card-number">
+                        <input id="number-cartao" type="text" placeholder="0000 0000 0000 0000" class="card-number"
+                        minlength="16" maxlength="16">
                     </div>
     
                     <div class="row">
                         <div class="input-grupo">
                             <label for="">Validade</label>
-                            <input id="validade-cartao" type="text" placeholder="MM/AA">
+                            <input id="validade-cartao" type="text" placeholder="MM/AA" minlength="4" maxlength="4">
                         </div>
     
                         <div class="input-grupo">
                             <label>CVV</label>
-                            <input id="CVV" type="text" placeholder="CVV" class="cvv">
+                            <input id="cvv" type="text" placeholder="CVV" class="cvv" minlength="3" maxlength="3">
                         </div>
                     </div>
     
                     <div class="input-grupo">
                         <label for="name">Titular do cartão</label>
-                        <input type="text" placeholder="Nome impresso no cartão" maxlength="200" onkeypress="return /[a-z\s]/i.test(event.key)">
+                        <input id="titular" type="text" placeholder="Nome impresso no cartão" maxlength="200" onkeypress="return /[a-z\s]/i.test(event.key)">
                     </div>
+                    <!--
+                        number-cartao
+                        validade-cartao
+                        cvv
+                        titular
+                    -->
                     <div>
-                        <button class="add-button" id="addButton">
+                        <!-- <button class="add-button" id="addButton">
                             <span class="button-text">CONFIRMAR</span>
                             <div class="loader"></div>
                             <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                                 <circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none" />
                                 <path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
                             </svg>
-                        </button>
+                        </button> -->
                     </div>
                 </div>
             </div>
