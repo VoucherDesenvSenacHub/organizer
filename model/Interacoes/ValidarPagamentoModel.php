99e2922d0ce941d50fd2c5ada5bf7eb638b519c0
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-10-23 21:19:20 -0400

    Criação da classe

--- model/Interacoes/ValidarPagamentoModel.php
+++ model/Interacoes/ValidarPagamentoModel.php
@@ -2,6 +2,56 @@
 require_once __DIR__ . "/../../config/database.php";
 class ValidarPagamentoModel
 {
-    
+    // Dados do cartão
+    public $numeroCartao;
+    public $nomeCartao;
+    public $expiracaoMes;
+    public $expiracaoAno;
+    public $cvv;
 
+    // Dados do produto
+    public $descricaoProduto;
+    public $valorProduto;
+
+    // Dados pessoais e de contato
+    public $nome;
+    public $cpfCnpj;
+    public $email;
+    public $cep;
+    public $enderecoNumero;
+    public $enderecoComplemento;
+    public $telefone;
+
+    // Construtor opcional (pode facilitar a inicialização)
+    public function __construct(
+        $numeroCartao = null,
+        $nomeCartao = null,
+        $expiracaoMes = null,
+        $expiracaoAno = null,
+        $cvv = null,
+        $descricaoProduto = null,
+        $valorProduto = null,
+        $nome = null,
+        $cpfCnpj = null,
+        $email = null,
+        $cep = null,
+        $enderecoNumero = null,
+        $enderecoComplemento = null,
+        $telefone = null
+    ) {
+        $this->numeroCartao = $numeroCartao;
+        $this->nomeCartao = $nomeCartao;
+        $this->expiracaoMes = $expiracaoMes;
+        $this->expiracaoAno = $expiracaoAno;
+        $this->cvv = $cvv;
+        $this->descricaoProduto = $descricaoProduto;
+        $this->valorProduto = $valorProduto;
+        $this->nome = $nome;
+        $this->cpfCnpj = $cpfCnpj;
+        $this->email = $email;
+        $this->cep = $cep;
+        $this->enderecoNumero = $enderecoNumero;
+        $this->enderecoComplemento = $enderecoComplemento;
+        $this->telefone = $telefone;
+    }
 }
