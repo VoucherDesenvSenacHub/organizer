8b01a07e41e730330a59fba7fafb33d3da22ddcf
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-11-13 21:21:15 -0400

    Separação de regras para o método

--- service/RelatorioService.php
+++ service/RelatorioService.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once  __DIR__ . "/../model/RelatoriosModel.php";
+require_once __DIR__ . '/../model/OngModel.php';
 
 class RelatorioService
 {
@@ -37,7 +38,7 @@ class RelatorioService
     }
 
     public function gerarVoluntariosProjeto($idOng)
-    {   
+    {
         ob_start();
         include __DIR__ . '/../report/voluntariosProjeto.php';
         $html = ob_get_clean();
@@ -49,6 +50,55 @@ class RelatorioService
 
     public function gerarDoacoesMensais($idOng)
     {
+        $ongs = new OngModel();
+        $relatorio = new RelatoriosModel();
+        $doacoes = $ongs->buscarDoadores($idOng);
+        $listaDoacoes = $ongs->dashboardOng($idOng);
+        $dadosDasTabelas = $relatorio->listarDadosTabela($idOng);
+        $year = date('Y');
+        $projetos = new RelatoriosModel();
+        $contagem_projetos = $projetos->contarProjetos($idOng);
+        $listagem_projetos = $projetos->listarProjetos($idOng);
+        $tabela = "";
+        $mesPorExtenso = [
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro'
+        ];
+        $totalAnual = 0;
+        for ($month = 1; $month <= 12; $month++):
+            $arrecadacao = $relatorio->painelDeArrecadacao($idOng, $month, $year);
+            if ($arrecadacao['total_doado'] === null) {
+                $valorTotal = 0;
+            } else {
+                $valorTotal = (float)$arrecadacao['total_doado'];
+                $totalAnual += $valorTotal;
+            }
+            $valorTotal = number_format($valorTotal, 2, ',', '.');
+            $mes = $mesPorExtenso[$month - 1];
+            $tabela = $tabela . "
+        <tr>
+        <td class='mes'>
+            $mes
+        </td>
+        <td class='valor'>
+            $valorTotal
+        </td>
+        </tr>
+    ";
+        endfor;
+
+        //Geração do PDF
+        
         ob_start();
         include __DIR__ . '/../report/doacoesMensais.php';
         $html = ob_get_clean();
@@ -60,6 +110,51 @@ class RelatorioService
 
     public function gerarDoacoesProjeto($idOng)
     {
+        $relatorio = new RelatoriosModel();
+        $arrecadacao = $relatorio->somarArrecadacaoProjetos($idOng);
+        $totalGeral = 0;
+        foreach ($arrecadacao as $a):
+            $totalGeral += $a['total_doacoes'];
+        endforeach;
+        $totalRodape = "R$ " . number_format($totalGeral, 2, ',', '.');
+        $tabela = "";
+        foreach ($arrecadacao as $a):
+            $nomeDoProjeto = $a['nome_projeto'];
+            $valorArrecadado = "R$ " . number_format($a['total_doacoes'], 2, ',', '.');
+            if ($totalGeral != 0) {
+                $percentual = number_format($a['total_doacoes'] * 100 / $totalGeral, 2) . "%";
+            } else {
+                $percentual = "0%";
+            }
+            $tabela = $tabela . "
+                <tr>
+                <td class='projeto'>
+                $nomeDoProjeto
+                </td>
+                <td class='valor'>
+                $valorArrecadado
+                </td>
+                <td class='valor'>
+                $percentual
+                </td>
+                </tr>
+                ";
+        endforeach;
+        $rodape = "
+                <td class='projeto'>
+                    Total arrecadado:
+                </td>
+                <td class='valor'>
+                    $totalRodape
+                </td>
+                <td class='valor'>
+                    ---
+                </td>
+                ";
+
+
+        // Geração do PDF
+
         ob_start();
         include __DIR__ . '/../report/doacoesProjeto.php';
         $html = ob_get_clean();
