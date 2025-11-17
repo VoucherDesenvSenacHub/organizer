1e11bc8c668ed7808f22e38e643a8bfb67988e1c
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-11-17 19:43:18 -0400

    Refatoração - A execução do 'Set Time Zone' ficou a cargo somente do arquivo de configuração do database (database.php)

--- config/database.php
+++ config/database.php
@@ -3,19 +3,15 @@
     $port = 28838;
     $dbname = 'organizer';
     $user = 'root';
-    $password = 'GxbLQsKICwpVBaOQegbVxZvLHfgSIorV'; 
+    $password = 'GxbLQsKICwpVBaOQegbVxZvLHfgSIorV';
 
-    // BANCO DE DADOS LOCAL (PARA TESTES):
-        // $host = 'localhost';
-        // $port = 3306;
-        // $dbname = 'organizer';
-        // $user = 'root';
-        // $password = '';
+    
     
     try {
         $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // echo "Conexão bem-sucedida!";
+        $pdo->exec("SET time_zone = '-04:00'");
     } catch (PDOException $e) {
         echo "Erro na conexão: " . $e->getMessage();
     }
