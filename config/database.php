dbb714ea8c3347c1324deb453be1c6d8a8ec4923
Geanoff <geancorrea.ga@gmail.com>
2025-11-25 08:56:34 -0400

    Salvando

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@
     // BANCO DE DADOS LOCAL (PARA TESTES):
         $host = 'localhost';
         $port = 3306;
-        $dbname = 'organizer2';
+        $dbname = 'organizer';
         $user = 'root';
         $password = '';
     
