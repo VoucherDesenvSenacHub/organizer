83bb9ccb4dea738adfc8d0ee86d65a6ba429837b
Luizfelipereal007 <lmarianobonfim@gmail.com>
2025-11-25 19:26:08 -0400

    refactor(config): :hammer: refactor database config to use environment variables

--- config/database.php
+++ config/database.php
@@ -1,22 +1,21 @@
-<?php 
-    $host = 'centerbeam.proxy.rlwy.net';
-    $port = 28838;
-    $dbname = 'organizer';
-    $user = 'root';
-    $password = 'GxbLQsKICwpVBaOQegbVxZvLHfgSIorV'; 
+<?php
+require __DIR__ . '/../vendor/autoload.php';
 
-    // BANCO DE DADOS LOCAL (PARA TESTES):
-        // $host = 'localhost';
-        // $port = 3306;
-        // $dbname = 'organizer';
-        // $user = 'root';
-        // $password = '';
+use Symfony\Component\Dotenv\Dotenv;
+
+(new Dotenv())->load(__DIR__ . '/../.env');
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']};charset=utf8",
+        $_ENV['DB_USERNAME'],
+        $_ENV['DB_PASSWORD'],
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
     
-    try {
-        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "Conexão bem-sucedida!";
-    } catch (PDOException $e) {
-        echo "Erro na conexão: " . $e->getMessage();
-    }
-?>
+    $GLOBALS['pdo'] = $pdo;
+    
+} catch (PDOException $e) {
+    error_log($e->getMessage());
+    die("Erro ao conectar ao banco de dados");
+}
